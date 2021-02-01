fdb8d7be13bdab652350ec2e7b9c4e6e484f2ced
den <pyshkodzianis@gmail.com>
2021-02-01 19:16:51 +0300

    gitignore commite

--- src/HomeWork31.java
+++ src/HomeWork31.java
@@ -3,24 +3,28 @@ import java.util.Random;
 
 public class HomeWork31  {
 
+
     public static void main(String[] args) {
-       // Boolean b = Boolean.TRUE;
 
         Random random = new Random();
         for (int i = 0; i < 100; i++) {
             int number = random.nextInt(1000);
 
-            String str1=new String(""+number);
+            String str1 = ("" + number);
 
-        String[]arrSplit=str1.split("\s",100);
+            String[] arrSplit = str1.split("\s", 100);
 
-            for(int j=0;j<arrSplit.length;j++)
-               System.out.print(""+arrSplit[j]);
+            for (int j = 0; j < arrSplit.length; j++)
+                System.out.print("" + arrSplit[j]);
             System.out.print(" ");
+
+         //   Random random1 = new Random();
+           // for (int k = 0; k < 100; k++) {
+             //   int number1 = random1.nextInt(1000);
+               // StringBuilder str2 = new StringBuilder(number1);
+            }
         }
-
-
-      }
     }
+
 
 
--- src/HomeWork32.java
+++ src/HomeWork32.java
@@ -1,2 +1,23 @@
+import java.util.Random;
 public class HomeWork32 {
-}
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            int number = random.nextInt(1000);
+
+            String str1 = ("" + number);
+
+            String[] arrSplit = str1.split("\s", 100);
+            for (int k = 0; k < arrSplit.length; k++) ;
+
+
+                System.out.print("" + arrSplit[k]);
+                System.out.print(" ");
+                for (int ch = 10; ch < 100; ch++) ;
+                str1.replaceAll("ch", "-1");
+                System.out.println("Result:" + str1);
+
+        }
+    }}
