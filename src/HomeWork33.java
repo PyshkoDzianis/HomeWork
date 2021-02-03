ffdc9817b58b020b55a0ad374182769e05e49458
den <pyshkodzianis@gmail.com>
2021-02-04 00:20:24 +0300

    d

--- src/HomeWork33.java
+++ src/HomeWork33.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 public class HomeWork33 {
     public static void main(String[] args) {
 
-        Scanner name = new Scanner(System.in);
-            System.out.println("введите ваше имя :");
-      Scanner age=new Scanner(System.in) ;
-          System.out.println("введите ваш  возраст:");
+        Scanner sc1 = new Scanner(System.in);
+               System.out.println("введите ваше имя :");
+                   String name=sc1.nextLine();
 
-       String template="Your name %s,%s ";
-           System.out.println(String.format(template,"den","37"));
-              System.out.println(String.format(template,"name","age"));
-        String formatedLine=String.format(template,name,age);
-            System.out.println(formatedLine);
+            Scanner sc2=new Scanner(System.in) ;
+                System.out.println("введите ваш  возраст:");
+                   String age= sc2.nextLine();
+       String template="hello %s,%s ";
+
+               String formatedLine=String.format(template,name,age);
+           System.out.println("Hello"+formatedLine);
+
     }
 
 
