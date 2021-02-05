f50845791c68a6ad774cef3bce12e18b5bc9d1e6
den <pyshkodzianis@gmail.com>
2021-02-05 23:41:47 +0300

    f

--- src/HomeWork34.java
+++ src/HomeWork34.java
@@ -8,24 +8,34 @@ import java.util.*;
       public static void main(String[] args) {
 
           String str = "one cat two cat three cat";
-              String[] strArr = str.split("\\s");
-           ArrayList<String> wordsList = new ArrayList<String>();
+          String[] strArr = str.split("\\s");
+          System.out.println("checking string:"+str);
+             ArrayList<String> wordsList = new ArrayList<>();
+             ArrayList<Integer> numberEncounter = new ArrayList<>();
 
-              int numberCount = 1;
 
-              for(int i = 0; i<strArr.length;i++)
-
-              {
-                  for (int j = i + 1; j < strArr.length; j++) {
-                      if (strArr[i].equals(strArr[j]))
-                          numberCount++;
-                  }
-
-                  System.out.println(strArr[i] + " : " + numberCount);
-                  numberCount = 1;
+          for (int i = 0; i < strArr.length; i++) {
+              String word = strArr[i];
+              word = word.toLowerCase();
+              if (wordsList.contains(word)) {
+                  int position = wordsList.indexOf(word);
+          Integer number = numberEncounter.get(position);
+            int number_int = number.intValue();
+                  number_int++;
+                  number = new Integer(number_int);
+                  numberEncounter.set(position, number);
+              } else {
+                  wordsList.add(word);
+                  numberEncounter.add(new Integer(1));
               }
-              }
-
           }
+              for (int i = 0;i < wordsList.size(); i++) {
+                  System.out.println(i + "\t" + wordsList.get(i) + "\t"
+                          + numberEncounter.get(i));
+
+              }
+          }
+
+      }
 
 
