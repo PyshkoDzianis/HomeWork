3086a4993f42cf044bb28ec05db8c78fe79a3914
den <pyshkodzianis@gmail.com>
2021-02-07 12:40:42 +0300

    de

--- src/HomeWork31.java
+++ src/HomeWork31.java
@@ -8,23 +8,19 @@ public class HomeWork31  {
 
         Random random = new Random();
         for (int i = 0; i < 100; i++) {
-            int number = random.nextInt(1000);
+           int number = random.nextInt(1000);
 
             String str1 = ("" + number);
 
-            String[] arrSplit = str1.split("\s", 100);
+            String[] arrSplit = str1.split("\\s", 100);
 
             for (int j = 0; j < arrSplit.length; j++)
-                System.out.print("" + arrSplit[j]);
-            System.out.print(" ");
+                System.out.println("" + arrSplit[j]);
 
-         //   Random random1 = new Random();
-           // for (int k = 0; k < 100; k++) {
-             //   int number1 = random1.nextInt(1000);
-               // StringBuilder str2 = new StringBuilder(number1);
-            }
-        }
+          }
+       }
     }
+
 
 
 
