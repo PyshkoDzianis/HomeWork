b698030f99c658dcc5b0d3044f0dc9168bf9976d
den <pyshkodzianis@gmail.com>
2021-02-07 13:01:20 +0300

    de

---
+++ src/HomeWork31StringBuilder.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class HomeWork31StringBuilder {
+    public static void main(String[] args) {
+        Random random1 = new Random();
+        for (int k = 0; k < 100; k++) {
+            int number1 = random1.nextInt(1000);
+            String str = ("" + number1);
+
+            StringBuilder builder = new StringBuilder();
+               builder.append(str).append("\s");
+                 String result = builder.toString();
+                     System.out.print("" + result);
+        }
+    }
+}
