1d7d582229ed8cb029b74a7cce50e8e4f8d3dc25
den <pyshkodzianis@gmail.com>
2021-02-08 09:28:06 +0300

    g

---
+++ src/HomeWork11.java
@@ -0,0 +1,34 @@
+
+   public class HomeWork11 {
+
+                public static void main(String[] args) {
+               int i;
+               i = 1;
+
+               switch (i) {
+                   case 1:
+                       System.out.println("понедельник");
+                       break;
+                   case 2:
+                       System.out.println("вторник");
+                       break;
+                   case 3:
+                       System.out.println("среда");
+                       break;
+                   case 4:
+                       System.out.println("четверг");
+                       break;
+                   case 5:
+                       System.out.println("пятница");
+                       break;
+                   case 6:
+                       System.out.println("суббота");
+                       break;
+                   case 7:
+                       System.out.println("воскресенье");
+                       break;
+                   default:
+                       System.out.println("не является днем недели");
+               }
+           }
+}
