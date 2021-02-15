3706a9f0934beb1db9b475d7cbb7b0239339904d
den <pyshkodzianis@gmail.com>
2021-02-15 15:52:55 +0300

    some changes3

---
+++ src/domain/ReccurringTask.java
@@ -0,0 +1,18 @@
+package domain;
+
+  public class ReccurringTask extends HomeWork {
+
+      private int count;
+
+
+      public ReccurringTask(String name, String dueDate, int number, int count) {
+          super(name, dueDate, number);
+          this.count = count;
+      }
+
+      @Override
+      public String getHomeWorkType() {
+          return null;
+      }
+
+     }
